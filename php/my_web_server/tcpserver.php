94ccd9459abf8c1157e49cf6b5ae0c29fdb8f2a0
kamiokan <spherenov@gmail.com>
2022-03-21 16:47:22 +0900

    リクエストを受け取るだけのTCPサーバーPHP版を作った

---
+++ php/my_web_server/tcpserver.php
@@ -0,0 +1,41 @@
+<?php
+
+class TCPServer
+{
+    public function serve()
+    {
+        # サーバーを起動する
+        echo "=== サーバーを起動します ===" . PHP_EOL;
+
+        try {
+            # socketを生成
+            $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+            socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
+
+            # socketをlocalhostのポート8080番に割り当てる
+            socket_bind($socket, 'localhost', 8080);
+            socket_listen($socket, 10);
+
+            # 外部からの接続を待ち、接続があったらコネクションを確立する
+            echo "=== クライアントからの接続を待ちます ===" . PHP_EOL;
+            $msg_sock = socket_accept($socket);
+            socket_getsockname($msg_sock);
+            echo "=== クライアントとの接続が完了しました ===" . PHP_EOL;
+
+            # クライアントから送られてきたデータを取得する
+            $request = socket_read($msg_sock, 1024);
+
+            # クライアントから送られてきたデータをファイルに書き出す
+            file_put_contents('server_recv.txt', $request, LOCK_EX);
+
+            # 返事は特に返さず、通信を終了させる
+            socket_close($msg_sock);
+            socket_close($socket);
+        } finally {
+            echo "=== サーバーを停止します。 ===" . PHP_EOL;
+        }
+    }
+}
+
+$server = new TCPServer();
+$server->serve();
