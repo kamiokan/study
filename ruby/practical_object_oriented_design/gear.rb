99e16d043ecbee7e65af1da65144dfdfc7623b29
kamiokan <spherenov@gmail.com>
2022-04-10 10:44:44 +0900

    チェーンリングとコグの歯数からギア比を出すクラスを作った

---
+++ ruby/practical_object_oriented_design/gear.rb
@@ -0,0 +1,15 @@
+class Gear
+  attr_reader :chain_ring, :cog
+
+  def initialize(chain_ring, cog)
+    @chain_ring = chain_ring
+    @cog = cog
+  end
+
+  def ratio
+    chain_ring / cog.to_f
+  end
+end
+
+puts Gear.new(52, 11).ratio
+puts Gear.new(30, 27).ratio
