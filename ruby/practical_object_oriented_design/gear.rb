ef90c2decb923346fa600141331dd84426d493ab
kamiokan <spherenov@gmail.com>
2022-04-10 12:26:44 +0900

    車輪の直径*ギア比（ギアインチ）を取得できるメソッドを追加した

--- ruby/practical_object_oriented_design/gear.rb
+++ ruby/practical_object_oriented_design/gear.rb
@@ -1,15 +1,22 @@
 class Gear
-  attr_reader :chain_ring, :cog
+  attr_reader :chain_ring, :cog, :rim, :tire
 
-  def initialize(chain_ring, cog)
+  def initialize(chain_ring, cog, rim, tire)
     @chain_ring = chain_ring
     @cog = cog
+    @rim = rim
+    @tire = tire
   end
 
   def ratio
     chain_ring / cog.to_f
   end
+
+  def gear_inches
+    # タイヤはリムの周りを囲むので、直径を計算するためには2倍する
+    ratio * (rim + (tire * 2))
+  end
 end
 
-puts Gear.new(52, 11).ratio
-puts Gear.new(30, 27).ratio
+puts Gear.new(52, 11, 26, 1.5).gear_inches
+puts Gear.new(52, 11, 24, 1.25).gear_inches
